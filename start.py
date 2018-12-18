9428dc0493298d53279a23f0b1aa65aa1d0efebf
AdSHSO <44525339+AdSHSO@users.noreply.github.com>
2018-12-18 12:26:13 +0100

    added - now with unit tests

--- start.py
+++ start.py
@@ -11,8 +11,8 @@ import hashlib
 #typical bandit findings
 #>>> bandit -r <folder>
 #deprecated md5 will not be found by sonar...
-#password="123_x&5s" 
-#hash_object = hashlib.md5(b'123_x32&')
+password="123_x&5s" 
+hash_object = hashlib.md5(b'123_x32&')
 
 password = b"bobo"
 
---
+++ testPassword.py
@@ -0,0 +1,9 @@
+import unittest
+import Password
+
+class testPassword(unittest.TestCase):
+    def test1(self):
+        True
+
+if __name__ == '__main__':
+    unittest.main()
